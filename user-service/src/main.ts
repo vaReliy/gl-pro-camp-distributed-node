76db37499ad01f7234c0f9130f56b336caac18da
Valeriy Garbuzov <valeriy@i.ua>
2021-02-09 21:33:37 +0200

    add swagger

--- user-service/src/main.ts
+++ user-service/src/main.ts
@@ -1,9 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Users APi')
+    .setDescription('This API allows you to manipulate users data')
+    .setVersion('1.0')
+    .addTag('Users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT);
 }
--- user-service/src/users/dto/create-user.dto.ts
+++ user-service/src/users/dto/create-user.dto.ts
@@ -1,10 +1,18 @@
 import { IsNotEmpty, Length, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+  /**
+   * User login
+   * @example Ivan
+   */
   @IsNotEmpty()
   @MinLength(3)
   username: string;
 
+  /**
+   * User password
+   * @example q1w2e3r4t5
+   */
   @IsNotEmpty()
   @Length(6, 20)
   password: string;

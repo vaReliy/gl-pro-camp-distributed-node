9a2302fa554d70548465cf75fb869c547ac45129
Valeriy Garbuzov <valeriy@i.ua>
2021-02-09 20:24:09 +0200

    add validation pipes, nestjs config (.env)

--- user-service/src/users/dto/create-user.dto.ts
+++ user-service/src/users/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, Length, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
+  @MinLength(3)
   username: string;
+
+  @IsNotEmpty()
+  @Length(6, 20)
   password: string;
 }

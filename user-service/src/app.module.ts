78e1653d0d11ab7ac9bd8a9a2829ac3b71bc2e03
Valeriy Garbuzov <valeriy@i.ua>
2021-03-02 20:19:48 +0200

    remove nestjs config (.env), add docker (mongo, user-api)

--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -1,16 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    UsersModule,
-    ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.DB_URL),
-  ],
+  imports: [UsersModule, MongooseModule.forRoot(process.env.DB_URL)],
   controllers: [AppController],
   providers: [AppService],
 })

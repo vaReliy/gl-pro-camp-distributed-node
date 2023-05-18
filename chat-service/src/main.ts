09374ac1644a0f78ba7f4eb406fee7cc3f327d49
Valerii H <valeriy@i.ua>
2023-05-18 15:37:16 +0300

    Update docker configs

--- chat-service/src/main.ts
+++ chat-service/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true }); // fixme: temporary, for local dev only!
+  const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT);
 }
 bootstrap();
